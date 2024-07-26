46901f3625205ed11789459a9b3642d616789db8
176984219-myseneca <osokunbi1@myseneca.ca>
2024-07-26 14:37:31 -0400

    Done until lab7d

--- lab7e.py
+++ lab7e.py
@@ -14,8 +14,8 @@ class Time:
         self.second = second
 
     def __str__(self):
-    '''return a string representation for the object self'''
-    return  f'{self.hour:02d}:{self.minute:02d}:{self.second:02d}'
+        '''return a string representation for the object self'''
+        return  f'{self.hour:02d}:{self.minute:02d}:{self.second:02d}'
 
     def format_time(self):
         """Return time object (t) as a formatted string"""
